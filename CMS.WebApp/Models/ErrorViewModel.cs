eeb67a4f90aedf472576ad2b0afc39367a1ede3e
manhxuanpham <manhhoathuan@gmail.com>
2024-10-13 08:55:51 +0700

    init project

---
+++ CMS.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMS.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
