ec4bc6e3bd333c584df7f5ad88da1cf6c07088f5
manhxuanpham <manhhoathuan@gmail.com>
2024-10-30 09:08:52 +0700

    post category

---
+++ CMS.Core/Repository/IPostCategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using CMS.Core.Domain.Content;
+using CMS.Core.Models.Content;
+using CMS.Core.Models;
+using CMS.Data.SeedWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Core.Repository
+{
+    public interface IPostCategoryRepository : IRepository<PostCategory, Guid>
+    {
+        Task<PagedResult<PostCategoryDto>> GetAllPaging(string? keyword, int pageIndex = 1, int pageSize = 10);
+    }
+}
---
+++ CMS.Data/Repository/PostCategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CMS.Core.Domain.Content;
+using CMS.Core.Models.Content;
+using CMS.Core.Models;
+using CMS.Core.Repository;
+using CMS.Data.SeedWorks;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Repository
+{
+    public class PostCategoryRepository : RepositoryBase<PostCategory, Guid>, IPostCategoryRepository
+    {
+        private readonly IMapper _mapper;
+        public PostCategoryRepository(CMSContext context, IMapper mapper) : base(context)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<PagedResult<PostCategoryDto>> GetAllPaging(string? keyword, int pageIndex = 1, int pageSize = 10)
+        {
+            var query = _context.PostCategories.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                query = query.Where(x => x.Name.Contains(keyword));
+            }
+            var totalRow = await query.CountAsync();
+
+            query = query.OrderByDescending(x => x.DateCreated)
+               .Skip((pageIndex - 1) * pageSize)
+               .Take(pageSize);
+
+            return new PagedResult<PostCategoryDto>
+            {
+                Results = await _mapper.ProjectTo<PostCategoryDto>(query).ToListAsync(),
+                CurrentPage = pageIndex,
+                RowCount = totalRow,
+                PageSize = pageSize
+            };
+        }
+    }
+}
